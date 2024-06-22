a4503417085e1a8e82d3b0dcedb530522bb76cdf
rostislavsolovsky <155907148+rostislavsolovsky@users.noreply.github.com>
2024-06-22 16:28:49 +0200

    Add files via upload

---
+++ zlomky2024 A/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Obecné informace o sestavení se řídí přes následující 
+' sadu atributů. Změnou hodnot těchto atributů se upraví informace
+' přidružené k sestavení.
+
+' Zkontrolujte hodnoty atributů sestavení.
+
+<Assembly: AssemblyTitle("zlomky2024")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("zlomky2024")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Následující GUID se používá pro ID knihovny typů (typelib), pokud je tento projekt vystavený pro COM.
+<Assembly: Guid("4a5f5feb-819d-4a2a-bcd9-56ae614dbea9")>
+
+' Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+'
+'      Hlavní verze
+'      Podverze
+'      Číslo sestavení
+'      Revize
+'
+' Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+' pomocí zástupného znaku * takto:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
